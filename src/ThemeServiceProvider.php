cccd6c6813af633b73a3b6f345d2c4a65af116a7
Stephen Lake <stephen@closurecode.com>
2019-04-28 17:30:41 +0000

    Apply fixes from StyleCI

--- src/ThemeServiceProvider.php
+++ src/ThemeServiceProvider.php
@@ -2,11 +2,11 @@
 
 namespace NovaSnowball;
 
-use Laravel\Nova\Nova;
-use Laravel\Nova\Events\ServingNova;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Nova\Events\ServingNova;
+use Laravel\Nova\Nova;
 
-class SnowballServiceProvider extends ServiceProvider
+class ThemeServiceProvider extends ServiceProvider
 {
     /**
      * Bootstrap snowball.
