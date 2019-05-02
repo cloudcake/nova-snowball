89c53173a5c2c6ed45f327ec7f7fd32d1b12f79a
Stephen Lake <stephen@closurecode.com>
2019-05-03 01:22:13 +0200

    Add support for collapsing resources menu item

--- resources/js/theme-sidebar-collapse.js
+++ resources/js/theme-sidebar-collapse.js
@@ -1,12 +1,86 @@
-window.addEventListener('load', function() {
-  const sidebarGroups = document.querySelectorAll('.w-sidebar h4:not(.expanded)')
+var snowballSidebarResourceGroups;
+var snowballSidebarHeadings;
+var snowballSidebarMainMenus;
 
-  for (var i = 0; i < sidebarGroups.length; i++) {
-    sidebarGroups[i].addEventListener('click', function() {
+window.addEventListener('load', function() {
+  snowballSidebarMainMenus = document.querySelectorAll('.w-sidebar h3:not(.expanded)')
+  snowballSidebarResourceGroups = document.querySelectorAll('.w-sidebar h4:not(.expanded)')
+  snowballSidebarHeadings = document.querySelectorAll('.w-sidebar h3:not(.cursor-pointer)')
+
+  registerMainMenusNavigation()
+  registerResourceGroupsNavigation()
+  registerResourceActiveLinks()
+})
+
+function registerMainMenusNavigation() {
+  snowballSidebarMainMenus.forEach(function(group) {
+    group.addEventListener('click', function() {
       var thisWasExpanded = this.classList.contains('expanded')
 
-      for (var x = 0; x < sidebarGroups.length; x++) {
-        sidebarGroups[x].classList.remove('expanded')
+      snowballSidebarMainMenus.forEach(function(otherGroup) {
+        otherGroup.classList.remove('expanded')
+      })
+
+      snowballSidebarResourceGroups.forEach(function(otherGroup) {
+        otherGroup.classList.remove('expanded')
+      })
+
+      if (!thisWasExpanded) {
+        this.classList.remove('expanded')
+      } else {
+        this.classList.add('expanded')
+      }
+    })
+  })
+}
+
+function registerResourceGroupsNavigation() {
+  snowballSidebarResourceGroups.forEach(function(group) {
+    group.addEventListener('click', function() {
+      var thisWasExpanded = this.classList.contains('expanded')
+
+      snowballSidebarResourceGroups.forEach(function(otherGroup) {
+        otherGroup.classList.remove('expanded')
+      })
+
+      if (thisWasExpanded) {
+        this.classList.remove('expanded')
+      } else {
+        this.classList.add('expanded')
+      }
+    })
+  })
+}
+
+function registerResourceActiveLinks() {
+
+  snowballSidebarHeadings.forEach(function(sidebarHeading) {
+
+    if ((activeLink = document.querySelector('.w-sidebar ul .router-link-active'))) {
+      const nearestUnorderedList = activeLink.closest('ul')
+
+      if (nearestUnorderedList && nearestUnorderedList.previousElementSibling) {
+        nearestUnorderedList.previousElementSibling.classList.add('expanded')
+
+        var sibling = nearestUnorderedList.previousElementSibling
+
+        for (var z = 0; z < nearestUnorderedList.parentNode.childNodes.length; z++) {
+          if (sibling.tagName == 'H3') {
+            sibling.classList.add('expanded')
+            break;
+          }
+
+          sibling = sibling.previousElementSibling
+        }
+      }
+    }
+
+    sidebarHeading.classList.add('cursor-pointer')
+    sidebarHeading.addEventListener('click', function() {
+      var thisWasExpanded = this.classList.contains('expanded')
+
+      for (var x = 0; x < snowballSidebarResourceGroups.length; x++) {
+        snowballSidebarResourceGroups[x].classList.remove('expanded')
       }
 
       if (thisWasExpanded) {
@@ -15,15 +89,5 @@ window.addEventListener('load', function() {
         this.classList.add('expanded')
       }
     })
-  }
-
-  var activeSidebarGroup = document.querySelector('.w-sidebar .router-link-active');
-
-  if (activeSidebarGroup) {
-    var nearestList = activeSidebarGroup.closest('ul')
-
-    if (nearestList && nearestList.previousElementSibling) {
-      nearestList.previousElementSibling.classList.add('expanded')
-    }
-  }
-})
+  })
+}
